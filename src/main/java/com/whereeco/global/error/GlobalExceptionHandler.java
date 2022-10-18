fe20d3ea691da4356f94f19e4e014aec2d44437a
vact19 <chj6703@gmail.com>
2022-10-18 19:06:52 +0900

    feat: ExceptionHandler 임시 비활성화

--- src/main/java/com/whereeco/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/whereeco/global/error/GlobalExceptionHandler.java
@@ -12,25 +12,25 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-@RestControllerAdvice
-@Slf4j
-public class GlobalExceptionHandler {
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String, String>> handleException(Exception e){
-        Map<String ,String> returnMap = new HashMap<>();
-        StringWriter sw = new StringWriter();
-        PrintWriter pw = new PrintWriter(sw);
-        e.printStackTrace(pw);
-        returnMap.put("errorMessage", e.getMessage());
-        returnMap.put("trace", sw.toString());
-
-
-        return ResponseEntity.internalServerError().body(returnMap);
-    }
-}
+//import java.util.Map;
+//
+//@RestControllerAdvice
+//@Slf4j
+//public class GlobalExceptionHandler {
+//
+//    @ExceptionHandler(Exception.class)
+//    public ResponseEntity<Map<String, String>> handleException(Exception e){
+//        Map<String ,String> returnMap = new HashMap<>();
+//        StringWriter sw = new StringWriter();
+//        PrintWriter pw = new PrintWriter(sw);
+//        e.printStackTrace(pw);
+//        returnMap.put("errorMessage", e.getMessage());
+//        returnMap.put("trace", sw.toString());
+//
+//
+//        return ResponseEntity.internalServerError().body(returnMap);
+//    }
+//}
 
 
 
